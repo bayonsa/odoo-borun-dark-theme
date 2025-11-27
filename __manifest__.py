9d526062a6f3350dcea8cd4cf0cac4bc360e90ec
Arman Bayounsa <arman.bayonsa@gmail.com>
2025-11-27 17:03:55 +0000

    Initial lightweight Borun dark backend theme

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Borun Dark Backend",
+    "summary": "Lightweight dark theme for Odoo backend (Borun Studios)",
+    "version": "1.0.0",
+    "category": "Theme/Backend",
+    "author": "Borun Studios",
+    "website": "https://borunstudios.co.uk",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "borun_dark_backend/static/src/scss/borun_dark.scss"
+        ]
+    },
+    "installable": True,
+    "application": False
+}
